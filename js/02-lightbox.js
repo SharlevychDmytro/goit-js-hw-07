25d4c611338f679648555c77ebfb5cbf4bcfa0fd
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-23 00:22:16 +0300

    add task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+let markupPage = "";
+
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+};
+
+galleryItems.forEach(({ preview, original, description }) => {
+  markupPage += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+});
+
+addMarkupPage(markupPage);
+
+function addMarkupPage(markup) {
+  refs.galleryEl.insertAdjacentHTML("beforeend", markup);
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
