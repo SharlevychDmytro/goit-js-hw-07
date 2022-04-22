4d2d6fe4c0868b1c887f8900d05f16a0ab872fe6
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-22 22:09:00 +0300

    add task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+};
+
+refs.galleryEl.addEventListener("click", onClickImage);
+
+addImagesMarkup(createImagesMarkup(galleryItems));
+
+function onClickImage(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  const sourceImg = e.target.dataset.source;
+  const alt = e.target.alt;
+  openModalImg(sourceImg, alt);
+}
+
+function createImagesMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+function addImagesMarkup(markup) {
+  refs.galleryEl.insertAdjacentHTML("beforeend", markup);
+}
+function openModalImg(url, alt) {
+  const instance = basicLightbox.create(`
+      <img src="${url}" alt="${alt}">
+  `);
+  instance.show();
+
+  refs.galleryEl.addEventListener(
+    "keyup",
+    (e) => {
+      if (e.code !== "Escape") {
+        return;
+      }
+      instance.close();
+    },
+    { once: true }
+  );
+}
